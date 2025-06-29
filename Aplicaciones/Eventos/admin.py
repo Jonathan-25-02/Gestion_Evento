091a300f8b1f660d52cbf1251f109ececb4ce13c
Jonathan <alayj3665@gmail.com>
2025-06-29 12:22:11 -0500

    crwacion de los admin.py

--- Aplicaciones/Eventos/admin.py
+++ Aplicaciones/Eventos/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Carrera, ModalidadEvento, Usuario, Evento, EstadoInscripcion, Inscripcion, ArchivoRequisito, Asistencia,Certificado,Notificacion
 
-# Register your models here.
+
+admin.site.register(Carrera)
+admin.site.register(ModalidadEvento)
+admin.site.register(Usuario)
+admin.site.register(Evento)
+admin.site.register(EstadoInscripcion)
+admin.site.register(Inscripcion)
+admin.site.register(ArchivoRequisito)
+admin.site.register(Asistencia)
+admin.site.register(Certificado)
+admin.site.register(Notificacion)
